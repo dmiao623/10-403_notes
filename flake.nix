b778e2edfe81952ff23d9c77f47201768be35fd8
dustinm <dustinmmiao@gmail.com>
2026-04-23 10:30:00 -0500

    Added dependency management with nix/uv

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "10-403 notes devenv";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = { self, nixpkgs }: 
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        texliveFull
+        texlivePackages.enumitem
+        uv
+      ];
+    };
+  };
+}
